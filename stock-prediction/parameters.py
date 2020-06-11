464b7d07d16f60dcb329a757f26b8804e089d2e9
asmcos <asmcos@gmail.com>
2020-06-11 15:11:19 +0800

    增加 stock prediction

---
+++ stock-prediction/parameters.py
@@ -0,0 +1,50 @@
+import os
+import time
+from tensorflow.keras.layers import LSTM
+
+
+# Window size or the sequence length
+N_STEPS = 70
+# Lookup step, 1 is the next day
+LOOKUP_STEP = 1
+
+# test ratio size, 0.2 is 20%
+TEST_SIZE = 0.2
+# features to use
+#FEATURE_COLUMNS = ["adjclose", "volume", "open", "high", "low"]
+FEATURE_COLUMNS = ['open','high','low','close','volume']
+# date now
+date_now = time.strftime("%Y-%m-%d")
+
+### model parameters
+
+N_LAYERS = 2
+# LSTM cell
+CELL = LSTM
+# 256 LSTM neurons
+UNITS = 256
+# 40% dropout
+DROPOUT = 0.4
+# whether to use bidirectional RNNs
+BIDIRECTIONAL = False
+
+### training parameters
+
+# mean absolute error loss
+# LOSS = "mae"
+# huber loss
+LOSS = "huber_loss"
+OPTIMIZER = "adam"
+BATCH_SIZE = 64
+EPOCHS = 400
+
+# Apple stock market
+ticker = "TSingtao"
+#青岛啤酒
+ticker_code = '600600'
+startdate = '2020-01-01'
+ticker_data_filename = os.path.join("data", f"{ticker}_{date_now}.csv")
+# model name to save, making it as unique as possible based on parameters
+model_name = f"{date_now}_{ticker}-{LOSS}-{OPTIMIZER}-{CELL.__name__}-seq-{N_STEPS}-step-{LOOKUP_STEP}-layers-{N_LAYERS}-units-{UNITS}"
+if BIDIRECTIONAL:
+    model_name += "-b"
