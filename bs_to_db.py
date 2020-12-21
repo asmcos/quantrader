5b3e27d1eddc14121b5f5cf9f115b351a09844ad
asmcos <asmcos@gmail.com>
2020-12-21 15:23:20 +0800

    修改提交数据到服务器

--- bs_to_db.py
+++ bs_to_db.py
@@ -9,9 +9,9 @@ import click
 from datetime import datetime
 import dbmongo_stock
 import os
-
+import json
 import argparse
-
+import requests
 # 判断是否 是显示，还是重新下载数据计算
 # 数据每天只需要下载一次
 
@@ -50,6 +50,18 @@ def get_data(name,code, start, end, adj):
 
     dbmongo_stock.insertdayKs(name,code,data_list)
 
+def get_data_post_server(name,code,start,end,adj):
+	rs = bs.query_history_k_data_plus(code, 'date,open,high,low,close,volume', start_date=start, end_date=end,
+                                      frequency='d', adjustflag=adj)
+	datas = rs.get_data()
+	datas['name'] = name
+	datas['code'] = code
+	datas = datas.set_index('date')
+	datas = datas.to_json(orient='table')
+	jsondatas = json.loads(datas)['data']
+
+	requests.post("http://127.0.0.1:3000/updatedayk",json=jsondatas)
+
 #获取股票的名字和代码号
 def getstockinfo(stock):
     #2019-12-09,sz.002094,青岛金王,化工,申万一级行业
@@ -74,4 +86,4 @@ if __name__ == "__main__":
      for stock in stocklist:
         code ,name = getstockinfo(stock)
         print('正在获取',name,'代码',code)
-        get_data(name,code,"2010-01-01",today,"3")
+        get_data_post_server(name,code,"2010-01-01",today,"3")
