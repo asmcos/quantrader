47b69a774cd592e8b026f7d1520217dad893d9bc
asmcos <asmcos@gmail.com>
2020-12-28 16:52:22 +0800

    增加显示日期

--- bs_to_db.py
+++ bs_to_db.py
@@ -54,13 +54,16 @@ def get_data_post_server(name,code,start,end,adj):
 	rs = bs.query_history_k_data_plus(code, 'date,open,high,low,close,volume', start_date=start, end_date=end,
                                       frequency='d', adjustflag=adj)
 	datas = rs.get_data()
+	if len(datas) < 2:
+		return
+	print(len(datas),datas.date[datas.index[-1]])
 	datas['name'] = name
 	datas['code'] = code
 	datas = datas.set_index('date')
 	datas = datas.to_json(orient='table')
 	jsondatas = json.loads(datas)['data']
-
-	requests.post("http://127.0.0.1:3000/updatedayk",json=jsondatas)
+	
+	#requests.post("http://127.0.0.1:3000/stock/updatedayk",json=jsondatas)
 
 #获取股票的名字和代码号
 def getstockinfo(stock):
@@ -86,4 +89,4 @@ if __name__ == "__main__":
      for stock in stocklist:
         code ,name = getstockinfo(stock)
         print('正在获取',name,'代码',code)
-        get_data_post_server(name,code,"2010-01-01",today,"3")
+        get_data_post_server(name,code,"2020-12-15",today,"3")
