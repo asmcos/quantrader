4ba86e83db235a8ed1a0f6c61655604b34a612c5
asmcos <asmcos@gmail.com>
2021-01-21 08:59:59 +0800

    增加行业

--- sina_min_kline.py
+++ sina_min_kline.py
@@ -15,7 +15,7 @@ import re
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--offset", help="开始执行的位置",default='0')
-parser.add_argument("--listlen", help="每次获取的股票数",default=10)
+parser.add_argument("--listlen", help="每次获取的股票数",default=200)
 parser.add_argument("--resave", help="每次获取的股票数",default='0')
 args = parser.parse_args()
 
@@ -29,6 +29,7 @@ today = datetime.now().strftime('%Y-%m-%d')
 
 code_list = []
 csv_data = []
+industrydict = {}
 
 def create_clickable_code(code):
     code = code.replace(".","")
@@ -48,9 +49,9 @@ def get_data_fromjs(text):
 		if float(data[2]) != 0:
 			rise = (float(data[3]) - float(data[2])) * 100 / float(data[2]) 
 			rise = float2(rise)
-		datas.append([code,data[0],data[2],data[3],rise])
+		datas.append([code,data[0],data[2],data[3],rise,industrydict[code]])
 
-	df = pd.DataFrame(datas,columns=['code','name','昨日收盘','当前价','涨跌'])
+	df = pd.DataFrame(datas,columns=['code','name','昨日收盘','当前价','涨跌','行业'])
 	df = df.sort_values(by="涨跌",ascending=False)
 	df['code'] = df['code'].apply(create_clickable_code)
 	df['涨跌'] = df['涨跌'].apply(create_color_rise1)
@@ -80,9 +81,11 @@ def get_min_kdata_tail( ):
 def getstockinfo(stock):
     #2019-12-09,sz.002094,青岛金王,化工,申万一级行业
     # 时间，股票代码，名称，类别
-    d,code,name,skip1,skip2 = stock.split(',')
+    d,code,name,industry,skip2 = stock.split(',')
     code=code.replace(".","")
-    return code,name
+
+    industrydict[code]=industry
+    return code,name,industry
 
 
 
@@ -96,6 +99,8 @@ if not os.path.exists('./datas/stock_industry_check.csv'):
 stocklist = open('./datas/stock_industry_check.csv').readlines()
 stocklist = stocklist[1+int(offset):] #删除第一行
 
+for stock in stocklist:
+    code ,name,industry = getstockinfo(stock)
 
 if __name__ == "__main__":
      
@@ -103,7 +108,7 @@ if __name__ == "__main__":
      
 
         for stock in stocklist:
-           code ,name = getstockinfo(stock)
+           code ,name,industry = getstockinfo(stock)
            print('正在获取',name,'代码',code)
            get_min_kdata(code)
         get_min_kdata_tail( )
