fc38c4eaf09dbc52a7fd8119e7b8055ef278038c
asmcos <asmcos@gmail.com>
2021-02-21 22:54:06 +0800

    修改短期要涨的股票，增加基金参数

--- 10jqka_doctor.py
+++ 10jqka_doctor.py
@@ -39,11 +39,13 @@ def create_clickable_code(code):
     url_template= '''<a href="http://doctor.10jqka.com.cn/{code}/" target="_blank"><font color="blue">{code}</font></a>'''.format(code=code)
     return url_template
 
-def get_stats_value(code,name,industry,key):
+#key 是所搜的关键词，例如：涨
+#count 是多少只基金购买了该股票
+def get_stats_value(code,name,industry,key,count):
     p = os.popen("cd datas/doctor/ ; grep '"+key+"' stock_"+code+"_text.csv;cd ../../")
-    count = p.read()
-    if len(count) > 20:
-        result_list.append([name,code,industry])
+    content = p.read()
+    if len(content) > 20:
+        result_list.append([name,code,industry,int(count)])
 
 
 #获取股票的名字和代码号
@@ -63,6 +65,18 @@ if not os.path.exists('./datas/stock_industry_check.csv'):
 stocklist = open('./datas/stock_industry_check.csv').readlines()
 stocklist = stocklist[1+int(offset):] #删除第一行
 
+if not os.path.exists('./datas/stockcount.csv'):
+    print('请先计算股票基金持股数....')
+    os.system('python3 10jqka_fund.py')
+
+stockfund = open('./datas/stockcount.csv').readlines()
+stockfund = stockfund[1+int(offset):] #删除第一行
+stockdict = {}
+
+for stock in stockfund:
+    skip0,code,skip2,count,skip4 = stock.split(',')
+    stockdict[code] = count
+
 key = "股价短线上涨概率较大"
 #key = "该股长期投资价值较高"
 
@@ -70,8 +84,10 @@ for stock in stocklist:
     code,name,skip1 = getstockinfo(stock)
     #get_doctor_html(code,name)
     #time.sleep(0.5)
-    get_stats_value(code,name,skip1,key)
+    count = stockdict[code]
+    get_stats_value(code,name,skip1,key,count)
 
-df = pd.DataFrame(result_list,columns=['name','code','行业'])
+df = pd.DataFrame(result_list,columns=['name','code','行业','fund'])
 df['code']=df['code'].apply(create_clickable_code)
+df = df.sort_values(by="fund",ascending=False)
 print(df.to_html(escape=False))
