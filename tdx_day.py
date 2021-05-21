970dfa138ae6cfc7ac2df988ccd9d7e878f3dce3
asmcos <asmcos@gmail.com>
2021-05-21 23:05:54 +0800

    修改返回值

--- tdx_day.py
+++ tdx_day.py
@@ -3,29 +3,34 @@
 from pytdx.hq import TdxHq_API
 import pandas as pd
 from common.framework import * 
-from common.common import today
+from common.common import endday 
 import json
 
 api = TdxHq_API()
 
 
 def get_bar(name,code):
-    zone,code = code.split('.') 
+    zone,code1 = code.split('.') 
 
     if zone == "sz":
         zone = 0
     if zone == "sh":
         zone = 1
     
-    print(name,code)
-    datas = api.get_security_bars(9,zone, str(code), 0, 20)
-    if datas == None :
-       return
-    df = api.to_df(datas)
-    #print(df)
-    print(len(df),df.datetime[df.index[-1]],df.iloc[0])
-    df = df.to_json(orient='table')
+    print(name,code1)
+    datas = api.get_security_bars(9,zone,code1, 0, 2)
+    datas = api.to_df(datas)
+    datas = datas.assign(date=datas['datetime'].apply(lambda x: str(x)[0:10])).drop(['year', 'month', 'day', 'hour', 'minute', 'datetime'], axis=1)
+    datas.rename(columns={'vol':'volume'},inplace = True)
+     
+    print(len(datas),datas.iloc[-1].date)
+    df = datas.to_json(orient='table')
     jsondatas = json.loads(df)['data']
+    for d in jsondatas:
+        d['name'] = name
+        d['code'] = code
+        del d['index']
+    #print(jsondatas)
 
 if api.connect('119.147.212.81', 7709):
 
