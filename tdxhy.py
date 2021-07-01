6183ae9f9437b1d33aee08e27ca702f0493b6b69
jiasheng he <jiashenghe@jiashengtekiMacBook-Air.local>
2021-07-01 18:05:12 +0800

    板块和板块下个股涨幅

---
+++ tdxhy.py
@@ -0,0 +1,249 @@
+#pytdx
+
+from pytdx.hq import TdxHq_API
+import pandas as pd
+from common.common import *
+
+api = TdxHq_API()
+
+tdxblockdf = ''
+tdxblockex = ''
+
+float2 = lambda a:float('%.2f' % a)
+
+
+block_list = []
+
+filename = './datas/stock_tdx_block'+endday+'.html'
+
+def get_block():
+    all_list = api.get_security_list(1, 0)
+    for i in all_list:
+        code  = int (i['code'])
+        if (code >= 880300) and (code <=880999) and (code != 880650):
+            #print(i['code'],i['name'])
+            block_list.append([i['code'],i['name']])
+dayK_list = []
+def get_blockbar():
+    for i in block_list:
+        code = i[0]
+        name = i[1]
+        datas = api.get_index_bars(9,1, code, 0, 20)
+        if datas == None or len(datas)<20:
+            continue
+        c1 = datas[-1]['close']
+        d1 = datas[-1]['datetime']
+        c2 = datas[-2]['close']
+        c5 = datas[-5]['close']
+        c10 = datas[-10]['close']
+        c20 = datas[-20]['close']
+        print(name,code,d1,c1,(c1-c2)*100/c2,(c1-c5)*100/c5,(c1-c10)*100/c10,(c1-c20)*100/c20)
+        dayK_list.append([name,code,d1,c1,(c1-c2)*100/c2,(c1-c5)*100/c5,(c1-c10)*100/c10,(c1-c20)*100/c20])
+
+    df = pd.DataFrame(dayK_list,columns=['name','code','date','close','今日涨幅','周涨幅','半月涨幅','月涨幅'])
+    df = df.sort_values(by='今日涨幅',ascending=False).reset_index()
+
+    df1 = df.iloc[:15]
+
+    df = df.sort_values(by='周涨幅',ascending=False).reset_index()
+    return df1, df.iloc[:15]
+    #del df['index']
+
+    #content += df.to_html(escape=False,float_format='%.2f')
+    #print("save file",filename)
+    #save_file(filename,content)
+
+
+
+
+def QA_fetch_get_tdx_industry() -> pd.DataFrame:
+    import random
+    import tempfile
+    import shutil
+    import os
+    from urllib.request import urlopen
+    global tdxblockdf
+
+    def gettempdir():
+        tmpdir_root = tempfile.gettempdir()
+        subdir_name = 'tdx_base' #+ str(random.randint(0, 1000000))
+        tmpdir = os.path.join(tmpdir_root, subdir_name)
+        if not os.path.exists(tmpdir): 
+            os.makedirs(tmpdir)
+
+        return tmpdir
+ 
+    def download_tdx_file(tmpdir) -> str:
+        url = 'http://www.tdx.com.cn/products/data/data/dbf/base.zip'
+        try:
+            file = tmpdir + '/' + 'base.zip'
+            f = urlopen(url)
+            data = f.read()
+            with open(file, 'wb') as code:
+                code.write(data)
+            f.close()
+            shutil.unpack_archive(file, extract_dir=tmpdir)
+            os.remove(file)
+        except:
+            pass
+        return tmpdir
+
+    def read_industry(folder:str) -> pd.DataFrame:
+        incon = folder + '/incon.dat' # tdx industry file
+        hy = folder + '/tdxhy.cfg' # tdx stock file
+
+        tbk = {}
+        # tdx industry file
+        with open(incon, encoding='GB18030', mode='r') as f:
+            incon = f.readlines()
+        incon_dict = {}
+        for i in incon:
+            if i[0] == '#' and i[1] != '#':
+                j = i.replace('\n', '').replace('#', '')
+                incon_dict[j] = []
+                start = 1
+            else:
+                if i[1] != '#':
+                    codelist = i.replace('\n', '').split(' ')[0].split('|')
+                    if len(codelist[0]) == 5 and codelist[0][0] == 'T':
+                        tbk[codelist[0]] = codelist[1]
+
+                    incon_dict[j].append(i.replace('\n', '').split(' ')[0].split('|'))
+
+        incon = pd.concat([pd.DataFrame.from_dict(v).assign(type=k) for k,v in incon_dict.items()]) \
+            .rename({0: 'code', 1: 'name'}, axis=1).reset_index(drop=True)
+
+        #for i in range(0,len(incon)):
+        #    print(incon.code.iloc[i],incon.name.iloc[i])
+
+        with open(hy, encoding='GB18030', mode='r') as f:
+            hy = f.readlines()
+        hy = [line.replace('\n', '') for line in hy]
+        hy = pd.DataFrame(line.split('|') for line in hy)
+        # filter codes
+        hy = hy[~hy[1].str.startswith('9')]
+        hy = hy[~hy[1].str.startswith('2')]
+
+        hy1 = hy[[1, 2]].set_index(2).join(incon.set_index('code')).set_index(1)[['name', 'type']]
+        hy2 = hy[[1, 3]].set_index(3).join(incon.set_index('code')).set_index(1)[['name', 'type']]
+
+        # add 56 tdx block
+        count = 0
+        hy['tbk1'] = ""
+        for i in hy[2].values:
+            if len(i) >=5:
+                hy['tbk1'].iloc[count] = tbk[i[:5]]
+            count += 1
+
+        # join tdxhy and swhy
+        df = hy.set_index(1) \
+            .join(hy1.rename({'name': hy1.dropna()['type'].values[0], 'type': hy1.dropna()['type'].values[0]+'_type'}, axis=1)) \
+            .join(hy2.rename({'name': hy2.dropna()['type'].values[0], 'type': hy2.dropna()['type'].values[0]+'_type'}, axis=1)).reset_index()
+
+        df.rename({0: 'sse', 1: 'code', 2: 'TDX_code', 3: 'SW_code'}, axis=1, inplace=True)
+        df = df[[i for i in df.columns if not isinstance(i, int) and  '_type' not in str(i)]]
+        df.columns = [i.lower() for i in df.columns]
+
+        #shutil.rmtree(folder, ignore_errors=True)
+        return df
+    folder = gettempdir()
+    dirpath = folder
+    if not os.path.exists(folder + '/incon.dat') or not os.path.exists(folder + '/tdxhy.cfg'): 
+        print("Save file to ",folder)
+        download_tdx_file(folder)
+
+    if len(tdxblockdf ) < 1000:
+        print("Read file from ",folder)
+        df = read_industry(folder)
+        tdxblockdf = df
+
+
+
+def get_bar(code,sse):
+    sse = int(sse)
+    code = str(code)
+    api.connect('119.147.212.81', 7709)
+
+    datas = api.get_security_bars(9,sse,code, 0, 10)
+    info = api.get_finance_info(sse, code)
+    datas = api.to_df(datas)
+
+    if len(datas) < 2:
+        return
+
+    liutonggu = float(info['liutongguben'])
+    close = datas.close.iloc[-1]
+    close1 = datas.close.iloc[-2]
+    close5 = datas.close.iloc[-5]
+    c1 = (close -close1) / close1
+    c5 = (close -close5) / close5
+    c1 = float(c1)*100
+    c5  = float(c5)*100
+    liutonggu  = liutonggu * close / 10000 / 10000
+    return (code,close,float2(c1),float2(c5),float2(liutonggu))
+
+api.connect('119.147.212.81', 7709)
+b = api.get_and_parse_block_info('block_gn.dat')
+hy1 = pd.DataFrame(b)
+
+# 获取板块
+QA_fetch_get_tdx_industry()
+hy = tdxblockdf
+
+def getmarket(code):
+    ret = hy.loc[hy.code == code,:]
+    return ret.sse.iloc[0]
+
+content = ""
+def sortblock(bklist,bkname,sse=0):
+    global content
+    result_list = []
+    if sse:
+        for i in range(0,len(bklist)):
+            result_list.append(
+                get_bar(bklist.code.iloc[i],getmarket(bklist.code.iloc[i]))
+               )
+    else:
+        for i in range(0,len(bklist)):
+            result_list.append(
+                get_bar(bklist.code.iloc[i],bklist.sse.iloc[i])
+               )
+
+
+    df = pd.DataFrame(result_list,columns=['code','close','今日涨幅','周涨幅','流通市值'])
+    df = df.sort_values(by='今日涨幅',ascending=False).reset_index()
+    del df['index']
+
+    content += '板块:' + bklist + '\n' 
+    content += df.to_html(escape=False,float_format='%.2f')
+    
+
+#尝试获取板块下面的股票列表
+def get_code_list(bkname):
+    print(bkname)
+    l1 =  hy1.loc[hy1.blockname == bkname,:]
+    if len(l1) > 0:
+        sortblock(l1,bkname,1)
+        return
+    
+    l =  hy.loc[hy.tdxnhy == bkname,:]
+    if len(l):
+        sortblock(l,bkname)
+        return
+
+    tbk1 = hy.loc[hy.tbk1 == bkname,:]
+    if len(tbk1):
+        sortblock(tbk1,bkname)
+        return
+
+
+get_block()
+df1,df2 = get_blockbar()
+
+for i in range(0,len(df1)):
+    get_code_list(df1.name.iloc[i])
+
+for i in range(0,len(df2)):
+    get_code_list(df2.name.iloc[i])
+
+print(content)
