b6049e4753c453323920928fe37626521a88cbfc
jiasheng he <jiashenghe@jiashengtekiMacBook-Air.local>
2021-11-18 03:50:54 +0800

    增加macd

--- transverse_tree.py
+++ transverse_tree.py
@@ -42,8 +42,8 @@ def main_loop(start,endday):
     if pred_data == 1:
         check_day = 0
 
-    for df in Kl.df_all[:500]:
-    #for df in Kl.df_all:
+    #for df in Kl.df_all[:500]:
+    for df in Kl.df_all:
 
         Kl.code(df["code"])
 
@@ -87,7 +87,9 @@ def main_loop(start,endday):
                 valc10 = C / ma10  
                 valc30 = C / ma30  
                 valc60 = C / ma60 
-   
+  
+                diff,dea,macd = MACD(C) 
+
                 HDAY = BARSLASTFIND(C,HHV(C,40));
                 if pred_data == 0:
                     maxc10 = talib.MAX(allC[-check_day-i:-i-1],check_day-1)[-1]
@@ -102,14 +104,14 @@ def main_loop(start,endday):
                 else:
                     label = 0
                 #print(C.data[-1],allC[-11-i],maxc10)
-                print(Kl.currentdf['name'],Kl.currentdf['code'],d,valc5,valc10,valc30,valc60,valc1,valh1,valo1,vall1,valv1,valv40,tran,HDAY,label)
-                all_list.append([Kl.currentdf['name'],Kl.currentdf['code'],d,valc5,valc10,valc30,valc60,valc1,valh1,valo1,vall1,valv1,valv40,tran,HDAY,label])
+                print(Kl.currentdf['name'],Kl.currentdf['code'],d,valc5,valc10,valc30,valc60,valc1,valh1,valo1,vall1,valv1,valv40,tran,HDAY,macd,label)
+                all_list.append([Kl.currentdf['name'],Kl.currentdf['code'],d,valc5,valc10,valc30,valc60,valc1,valh1,valo1,vall1,valv1,valv40,tran,HDAY,macd,label])
             except :
                 print("Klang ERROR",df['code'],df['name'])
 
                 PrintException()
 
-fields = ['name','code','日期','5日均线比','10日均线比','30日均线比','60日均线比','C涨幅','H涨幅','O涨幅','L涨幅','V涨幅','40日量比','60日震荡','40日新高','是否涨幅10%']
+fields = ['name','code','日期','5日均线比','10日均线比','30日均线比','60日均线比','C涨幅','H涨幅','O涨幅','L涨幅','V涨幅','40日量比','60日震荡','40日新高','macd','是否涨幅10%']
 
 
 main_loop(start=None,endday='2021-07-01')
