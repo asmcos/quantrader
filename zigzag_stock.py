66259f0b1ec9a51a5ae90af33f19bdb05bfd01b6
asmcos <asmcos@gmail.com>
2023-02-01 23:47:44 +0800

    设置宽度

--- zigzag_stock.py
+++ zigzag_stock.py
@@ -32,13 +32,14 @@ loaded_data = Kl.day_df
 #datetime types
 loaded_data['datetime'] = pd.to_datetime(loaded_data['datetime'])
 
-
+# Instantiate axes.
+(fig, ax) = plt.subplots( figsize=(21, 7) )
 
 def plot_pivots(X, pivots):
     plt.xlim(0, len(X))
     plt.ylim(X.min()*0.99, X.max()*1.01)
-    plt.plot(np.arange(len(X)), X, 'k:', alpha=0.5)
-    plt.plot(np.arange(len(X))[pivots != 0], X[pivots != 0], 'k-')
+    ax.plot(np.arange(len(X)), X, 'k:', alpha=0.5)
+    ax.plot(np.arange(len(X))[pivots != 0], X[pivots != 0], 'k-')
     plt.scatter(np.arange(len(X))[pivots == 1], X[pivots == 1], color='g')
     plt.scatter(np.arange(len(X))[pivots == -1], X[pivots == -1], color='r')
 
