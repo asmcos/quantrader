29673f606582e66d1a025814149ad0be520db60e
asmcos <asmcos@blx.com>
2023-02-03 13:44:22 +0800

    增加draw cup

--- zigzag_stock.py
+++ zigzag_stock.py
@@ -85,6 +85,10 @@ def pattern_cup_handle():
             cb / 3 > cd: 
             ax.text(a+1,close[a],"<-A")
             ax.text(c+1,close[c],"<-C")
+            ax.annotate('', xy=(c, close[c]),xytext=(b, close[b]),\
+                arrowprops=dict(facecolor='black', arrowstyle='-',connectionstyle="arc3,rad=0.4"))
+            ax.annotate('', xy=(b, close[b]),xytext=(a, close[a]),\
+                arrowprops=dict(facecolor='black', arrowstyle='-',connectionstyle="arc3,rad=0.4"))
             return 1
 
     return 0
